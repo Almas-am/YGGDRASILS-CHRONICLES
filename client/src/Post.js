14e2547b3c06de431ecbb21cef0d01fa29753b7a
Almas-am <amrishatiwari408@gmail.com>
2024-06-12 13:25:08 +0530

    Routing with react-router-dom

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return(
+        <div className="post">
+            <div className="Image">
+                <img src="https://techcrunch.com/wp-content/uploads/2024/06/WWDC-2024-Apple-Intelligence-Reveal.jpg?resize=768,432"></img>
+            </div>
+            <div className="text">
+                <h2>Here’s everything Apple announced at the WWDC 2024 keynote, including Apple Intelligence, Siri makeover</h2>
+                <p className="info">
+                <a className="author"></a>
+                <time>2024-06-11 16:45</time>
+                </p>
+                <p className="summary">It’s WWDC 2024 keynote time! Each year Apple kicks off its Worldwide Developers Conference with a few hours of just straight announcements, like the long-awaited Apple Intelligence and a makeover for smart AI assistant, Siri.</p>
+            </div>
+        </div>
+    );
+}
