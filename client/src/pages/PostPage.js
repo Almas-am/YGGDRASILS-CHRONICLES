d61f42764a46ffd5760ac30164cd6a33df7481f4
Almas-am <amrishatiwari408@gmail.com>
2024-07-08 17:19:55 +0530

    styling the post page

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import {formatISO9075} from "date-fns";
 
 export default function PostPage() {
   const [postInfo, setPostInfo] = useState(null);
@@ -43,11 +44,13 @@ export default function PostPage() {
 
   return (
     <div className="post-page">
+      <h1>{postInfo.title}</h1>
+      <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+      <div className="author">by @{postInfo.author.username}</div>
       <div className="image">
         <img src={`http://localhost:2000/${postInfo.cover}`} alt={postInfo.title} />
       </div>
-      <h1>{postInfo.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: postInfo.content }} />
+      <div className="content" dangerouslySetInnerHTML={{ __html: postInfo.content }} />
     </div>
   );
 }
